1c45ba333837ab414d3b8d4a1dd5104fddded4ba
sergio fores <sergiofores@gmail.com>
2025-02-19 18:27:04 +0100

    Permitir CORS desde github.io

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,13 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175",
+        "http://localhost:5176",
+        "https://t0t.github.io"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
